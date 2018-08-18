1559bdefa60f11ebb0522f9b330008549ceb4130
Lukasz <lukasz.reszke@netcompetence.se>
2018-08-18 17:01:20 +0200

    working solution

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
@@ -22,28 +27,55 @@ namespace SwaggerAuthDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme, JwtBearerDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme)
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             })
-                .AddJwtBearer(options =>
-                {
-                    options.Audience = $"{Configuration["AzureAd:Audience"]}";
-                    options.Authority = $"https://login.microsoftonline.com/{Configuration["AzureAd:DirectoryId"]}/";
-                });
+            .AddJwtBearer(options =>
+            {
+                options.Audience = $"{Configuration["AzureAd:Audience"]}";
+                options.Authority = $"https://login.microsoftonline.com/{Configuration["AzureAd:DirectoryId"]}/";
+            })
+            .AddAzureAd(options => Configuration.Bind("AzureAd", options))
+            .AddCookie();
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
                 c.AddSecurityDefinition("oauth2", new OAuth2Scheme
                 {
                     Type = "oauth2",
                     AuthorizationUrl = $"https://login.microsoftonline.com/{Configuration["AzureAd:DirectoryId"]}/oauth2/authorize",
                     TokenUrl = $"https://login.microsoftonline.com/{Configuration["AzureAd:DirectoryId"]}/oauth2/token",
-                    Flow = "implicit"
+                    Flow = "implicit",
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { "user_impersonation", "Access TenantManager.Api" }
+                    }
                 });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    { "oauth2", new string []  { "user_impersonation" } }
+                });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                                build => build.AllowAnyOrigin()
+                                    .AllowAnyMethod()
+                                    .AllowCredentials()
+                                    .AllowAnyHeader());
             });
         }
 
@@ -58,9 +90,8 @@ namespace SwaggerAuthDemo
             {
                 app.UseHsts();
             }
-
+            // ORDER MATTERS!
             app.UseHttpsRedirection();
-            app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -73,7 +104,13 @@ namespace SwaggerAuthDemo
                 {
                    { "resource", $"{Configuration["AzureAd:Audience"]}" }
                 });
+                c.OAuthScopeSeparator(" ");
             });
+            app.UseCors("CorsPolicy");
+    
+            app.UseAuthentication();
+            app.UseMvc();
+
         }
     }
 }
